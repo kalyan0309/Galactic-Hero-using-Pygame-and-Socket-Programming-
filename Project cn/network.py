da85de5017c3f3070a45c23d30ccf93c5099e208
CHITTAJALLU   LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2022-11-25 16:56:49 +0530

    Add files via upload

---
+++ Project cn/network.py
@@ -0,0 +1,28 @@
+import socket
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.156.83"
+        self.port = 8000
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+        self.values=[]
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
+
